0066fefbadd8f35e4016db4b5f4051a31b0629a5
Aleksandra <ola.otlowska@gmail.com>
2023-02-17 16:21:48 +0100

    ukol 1

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -7,11 +7,369 @@ public class HlavniProgram {
     public void start() {
         Turtle zofka = new Turtle();
 
-        //TODO implementace domácího úkolu
+        /*
+        // Cast 1
+        for (var i = 1; i < 2; i++) {
+            zofka.turnLeft(180);
+            zofka.move(100);
+            zofka.turnLeft(45);
+            zofka.move(30);
+            zofka.turnRight(180);
+            zofka.move(30);
+            zofka.turnLeft(90);
+            zofka.move(30);
+            zofka.turnLeft(180);
+            zofka.move(30);
+            zofka.turnLeft(45);
+            zofka.move(100);
+            zofka.turnLeft(90);
+            zofka.move(140);
+            zofka.turnLeft(90);
+            zofka.move(100);
+            zofka.turnRight(45);
+            zofka.move(30);
+            zofka.turnLeft(180);
+            zofka.move(30);
+            zofka.turnRight(90);
+            zofka.move(30);
+            zofka.turnRight(180);
+            zofka.move(100);
+            zofka.turnRight(90);
+            zofka.move(70);
+            zofka.turnRight(135);
+            zofka.move(100);
+            zofka.turnLeft(90);
+            zofka.move(140);
+            zofka.turnLeft(90);
+            zofka.move(100);
+        }
+
+
+        // Cast 2
+        zofka.penUp();
+        zofka.move(150);
+        zofka.penDown();
+
+
+        for (var i = 1; i < 9; i++) {
+            zofka.turnRight(45);
+            zofka.move(70);
+        }
+
+        zofka.turnLeft(90);
+        zofka.penUp();
+        zofka.move(100);
+        zofka.penDown();
+
+
+
+        for (var i = 1; i < 20; i++) {
+            zofka.turnRight(20);
+            zofka.move(20);
+        }
+
+        zofka.turnLeft(90);
+        zofka.penUp();
+        zofka.move(100);
+        zofka.penDown();
+
+
+        for (var i = 1; i < 20; i++) {
+            zofka.turnRight(20);
+            zofka.turnLeft(90);
+            zofka.move(15);
+            zofka.turnLeft(180);
+            zofka.move(15);
+            zofka.turnLeft(90);
+            zofka.move(20);
+        }
+        */
+
+
+        //Cast 3
+
+        for (var i = 1; i < 2; i++) {
+            zofka.turnLeft(180);
+            zofka.move(100);
+            zofka.turnLeft(45);
+            zofka.move(30);
+            zofka.turnRight(180);
+            zofka.move(30);
+            zofka.turnLeft(90);
+            zofka.move(30);
+            zofka.turnLeft(180);
+            zofka.move(30);
+            zofka.turnLeft(45);
+            zofka.move(100);
+            zofka.turnLeft(90);
+            zofka.move(140);
+            zofka.turnLeft(90);
+            zofka.move(100);
+            zofka.turnRight(45);
+            zofka.move(30);
+            zofka.turnLeft(180);
+            zofka.move(30);
+            zofka.turnRight(90);
+            zofka.move(30);
+            zofka.turnRight(180);
+            zofka.move(100);
+            zofka.turnRight(90);
+            zofka.move(70);
+            zofka.turnRight(135);
+            zofka.move(100);
+            zofka.turnLeft(90);
+            zofka.move(140);
+            zofka.turnLeft(90);
+            zofka.move(100);
+        }
+
+
+        zofka.turnRight(90);
+        zofka.penUp();
+        zofka.move(300);
+        zofka.turnRight(90);
+        zofka.move(100);
+        zofka.penDown();
+
+        for (var i = 1; i < 2; i++) {
+            zofka.turnLeft(180);
+            zofka.move(100);
+            zofka.turnRight(45);
+            zofka.move(80);
+            zofka.turnRight(90);
+            zofka.move(80);
+            zofka.turnRight(45);
+            zofka.move(100);
+            zofka.turnRight(90);
+            zofka.move(114);
+            zofka.turnRight(90);
+            zofka.move(100);
+            zofka.turnRight(90);
+            zofka.move(114);
+            zofka.turnLeft(90);
+        }
+
+
+        zofka.penUp();
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(114);
+        zofka.penDown();
+        zofka.turnRight(270);
+
+
+        for (var i = 1; i < 2; i++) {
+            zofka.turnLeft(180);
+            zofka.move(100);
+            zofka.turnRight(45);
+            zofka.move(80);
+            zofka.turnRight(90);
+            zofka.move(80);
+            zofka.turnRight(45);
+            zofka.move(100);
+            zofka.turnRight(90);
+            zofka.move(114);
+            zofka.turnRight(90);
+            zofka.move(100);
+            zofka.turnRight(90);
+            zofka.move(114);
+            zofka.turnLeft(90);
+        }
+
+
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(300);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.penDown();
+
+
+        for (var i = 1; i < 2; i++) {
+            zofka.turnLeft(180);
+            zofka.move(100);
+            zofka.turnRight(45);
+            zofka.move(80);
+            zofka.turnRight(90);
+            zofka.move(80);
+            zofka.turnRight(45);
+            zofka.move(100);
+            zofka.turnRight(90);
+            zofka.move(114);
+            zofka.turnRight(90);
+            zofka.move(100);
+            zofka.turnRight(90);
+            zofka.move(114);
+            zofka.turnLeft(90);
+        }
+
+
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(300);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.penDown();
+
+
+        for (var i = 1; i < 2; i++) {
+            zofka.turnLeft(180);
+            zofka.move(100);
+            zofka.turnRight(45);
+            zofka.move(80);
+            zofka.turnRight(90);
+            zofka.move(80);
+            zofka.turnRight(45);
+            zofka.move(100);
+            zofka.turnRight(90);
+            zofka.move(114);
+            zofka.turnRight(90);
+            zofka.move(100);
+            zofka.turnRight(90);
+            zofka.move(114);
+            zofka.turnLeft(90);
+        }
+
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(300);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.penDown();
+
+
+        for (var i = 1; i < 2; i++) {
+            zofka.turnLeft(180);
+            zofka.move(100);
+            zofka.turnRight(45);
+            zofka.move(80);
+            zofka.turnRight(90);
+            zofka.move(80);
+            zofka.turnRight(45);
+            zofka.move(100);
+            zofka.turnRight(90);
+            zofka.move(114);
+            zofka.turnRight(90);
+            zofka.move(100);
+            zofka.turnRight(90);
+            zofka.move(114);
+            zofka.turnLeft(90);
+        }
+
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(300);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.penDown();
+
+
+        for (var i = 1; i < 2; i++) {
+            zofka.turnLeft(180);
+            zofka.move(100);
+            zofka.turnRight(45);
+            zofka.move(80);
+            zofka.turnRight(90);
+            zofka.move(80);
+            zofka.turnRight(45);
+            zofka.move(100);
+            zofka.turnRight(90);
+            zofka.move(114);
+            zofka.turnRight(90);
+            zofka.move(100);
+            zofka.turnRight(90);
+            zofka.move(114);
+            zofka.turnLeft(90);
+        }
+
+        zofka.penUp();
+        zofka.move(65);
+        zofka.turnRight(90);
+        zofka.move(40);
+        zofka.penDown();
+
+        for (var i = 1; i < 19; i++) {
+            zofka.turnRight(20);
+            zofka.turnLeft(90);
+            zofka.move(10);
+            zofka.turnLeft(180);
+            zofka.move(10);
+            zofka.turnLeft(90);
+            zofka.move(10);
+
+        }
+
+        zofka.penUp();
+        zofka.move(65);
+        zofka.turnRight(90);
+        zofka.move(360);
+        zofka.turnRight(90);
+        zofka.move(220);
+        zofka.turnLeft(90);
+        zofka.penDown();
+
+
+        for (var i = 1; i < 2; i++) {
+            zofka.turnLeft(180);
+            zofka.move(100);
+            zofka.turnRight(45);
+            zofka.move(80);
+            zofka.turnRight(90);
+            zofka.move(80);
+            zofka.turnRight(45);
+            zofka.move(100);
+            zofka.turnRight(90);
+            zofka.move(114);
+            zofka.turnRight(90);
+            zofka.move(100);
+            zofka.turnRight(90);
+            zofka.move(114);
+            zofka.turnLeft(90);
+        }
+
+        zofka.penUp();
+        zofka.turnRight(180);
+        zofka.move(180);
+        zofka.turnLeft(90);
+        zofka.move(20);
+        zofka.turnLeft(90);
+        zofka.penDown();
+
+
+
+        for (var i = 1; i < 19; i++) {
+            zofka.turnRight(20);
+            zofka.move(20);
+        }
+
+        zofka.penUp();
+        zofka.turnRight(90);
+        zofka.move(200);
+        zofka.penDown();
+        zofka.turnLeft(90);
+        zofka.move(40);
+        zofka.turnLeft(180);
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(80);
+        zofka.penUp();
+        zofka.move(100);
+        zofka.penDown();
+        zofka.turnLeft(70);
+        zofka.move(100);
+        zofka.turnRight(140);
+        zofka.move(100);
+        zofka.turnRight(180);
+        zofka.move(50);
+        zofka.turnLeft(70);
+        zofka.move(30);
+
+
     }
 
-    public static void main(String[] args) {
-        new HlavniProgram().start();
-    }
 
-}
+        public static void main(String[] args) {
+            new HlavniProgram().start();
+        }
+
+    }
